23d3632fd4926b759c3a46c0c219e544ebc6378e
Rodolfo <ro_arteaga@javeriana.edu.co>
2018-10-10 18:30:52 -0500

    Proyecto por capas

---
+++ BAL/Campaigns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class Campaigns
+    {
+        
+        public List<DAL.Campaign> GetCampaigns()
+        {
+            DAL.touresBalonBDEntities db =new DAL.touresBalonBDEntities();
+            return db.Campaign.ToList();
+        }
+    }
+}
